0bfaef36ea3898e78c0f6531c326662faeb4c914
bret99 <64787351+bret99@users.noreply.github.com>
2021-04-03 23:28:08 +0400

    Update lightshot_image_scraper.py

--- lightshot_image_scraper.py
+++ lightshot_image_scraper.py
@@ -8,6 +8,7 @@ import random
 import string
 import os
 import sys
+import time
 
 options = FirefoxOptions()
 options.add_argument("--headless")
@@ -20,19 +21,13 @@ lang_list = ['eng', 'rus']
 links_list = []
 
 
-def generate_alphanum_random_string():
-    letters_and_digits = string.ascii_letters + string.digits
-    rand_string = ''.join(random.sample(letters_and_digits, 6)).lower()
-    return rand_string
-
-
 def user_input_secrets():
     while 1:
         user_input_secrets = input(
             '\nEnter the \033[1;91msecrets\033[1;00m to find: ')
         secrets_list.append(user_input_secrets)
         user_input_exit = input(
-                'Would you like to enter more \033[1;91msecrets\033[1;00m (y/n)? ')
+            'Would you like to enter more \033[1;91msecrets\033[1;00m (y/n)? ')
         if user_input_exit in positive_answers or user_input_exit == '':
             pass
         elif user_input_exit in negative_answers:
@@ -41,6 +36,15 @@ def user_input_secrets():
             break
         else:
             sys.exit('\033[1;91m\nWrong input!!!\nExiting..\033[1;00m\n')
+
+
+def generate_alphanum_random_string():
+    letters_and_digits = string.ascii_letters + string.digits
+    rand_string = ''.join(random.sample(letters_and_digits, 6)).lower()
+    return rand_string
+
+
+start = time.time()
 
 
 def main():
@@ -73,15 +77,19 @@ def main():
         scan_result = pytesseract.image_to_string(Image.open(
             '{}/image.png'.format(os.getcwd())),
                                                   lang=user_input_lang)
-        print(scan_result)
+        print('')
         for secret in secrets_list:
             if secret in scan_result:
                 print(
-                    "\033[1;91mSecret FOUND on \033[1;92m'https://prnt.sc/{0}'\033[1;91m! It is \033[1;92m{1}\033[1;00m"
+                    "[] \033[1;91mFOUND on \033[1;93m'https://prnt.sc/{0}'\033[1;91m! It is \033[1;92m{1}\033[1;00m"
                     .format(link, secret))
                 os.system(
                     "echo '[] Secret FOUND on https://prnt.sc/{0}! It is {1}' >> lightshot_scan_results.txt"
                     .format(link, secret))
+            elif secret not in scan_result:
+                print(
+                    '[] \033[1;94m{0}\033[1;00m not found on "https://prnt.sc/{1}"'
+                    .format(secret, link))
         print(
             "\n=================================================================================================================\n"
         )
@@ -101,14 +109,11 @@ if user_input_lang not in lang_list:
 else:
     user_input_secrets()
     main()
-    
-driver.quit()    
 
-print('Your links are:\n')
-
-for link in links_list:
-    print('\033[1;93mhttps://prnt.sc/{}\033[1;00m'.format(link))
+driver.quit()
 
 print(
     '\nIf SUCCESSFUL you will find scan results in \033[1;94mlightshot_scan_results.txt\033[1;00m\n'
 )
+print('Elapsed time is: \033[1;92m{}\033[1;00m seconds\n'.format(
+    int(time.time() - start)))
